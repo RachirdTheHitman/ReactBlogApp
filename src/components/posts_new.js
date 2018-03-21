bbb342bce2651419f13ac37ebcf5efba59944faa
YiZhang <hyacinthzy1@gmail.com>
2018-03-21 13:02:24 +1100

    router and form initial set

---
+++ src/components/posts_new.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <div>
+                PostsNew!
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
+
+// //PostsEdit.js
+// export default reduxForm({
+//     form: 'PostsNewForm'
+// })(PostsNew);
